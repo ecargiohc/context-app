fcda17446aafef5baf31a1f5a40354a7b57f282a
github username <therealgracechoi@gmail.com>
2020-03-02 21:03:59 -0800

    added PageContent.js to style App page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Form from './Form';
+import PageContent from './PageContent';
 
 function App() {
   return (
-    <>
+    <PageContent>
       <Navbar />
       <Form />
-    </>
+    </PageContent>
   );
 }
 
