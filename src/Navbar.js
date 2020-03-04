8b1e2fcaaf051d2ce2844e7953b782719a4f314e
github username <therealgracechoi@gmail.com>
2020-03-04 15:43:02 -0800

    imported ThemeContext to Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,9 +8,11 @@ import SearchIcon from '@material-ui/icons/Search';
 import Switch from '@material-ui/core/Switch';
 import {withStyles} from '@material-ui/core/styles';
 import styles from './styles/NavBarStyles';
+import {ThemeContext} from './contexts/ThemeContext';
 
 
 class Navbar extends Component {
+    static contextType = ThemeContext;
     render() {
         const {classes} = this.props;
         return(
