f4259bdb2b8ad78a061bd278a9668f8ea23a7fee
github username <therealgracechoi@gmail.com>
2020-03-08 09:34:55 -0700

    added new method to change Language in LanguageContext.js

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -6,10 +6,15 @@ export  class LanguageProvider extends Component {
     constructor(props) {
         super(props);
         this.state = {language: "french"}
+        this.changeLanguage = this.changeLanguage.bind(this);
+    };
+    changeLanguage(e) {
+        this.setState(e.target.value);
     };
     render() {
         return(
-            <LanguageProvider value={{...this.state}}>
+            <LanguageProvider 
+            value={{...this.state, changeLanguage: this.changeLanguage}}>
                 {this.props.children}
             </LanguageProvider>
         )
