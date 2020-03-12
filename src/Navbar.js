6725583653024180141e2af072f968dff41c1749
github username <therealgracechoi@gmail.com>
2020-03-12 11:36:31 -0700

    ommitted withLanguageContext wrapper from Navbar, leaving it with just a HOC and changing it into a functional component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useContext} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -9,7 +9,7 @@ import Switch from '@material-ui/core/Switch';
 import {withStyles} from '@material-ui/core/styles';
 import styles from './styles/NavBarStyles';
 import {ThemeContext} from './contexts/ThemeContext';
-import {withLanguageContext} from './contexts/LanguageContext';
+import {LanguageContext} from './contexts/LanguageContext';
 
 const content = {
     english: {
@@ -25,11 +25,12 @@ const content = {
         flag: "🇪🇸"
     }
 };
-class Navbar extends Component {
-    static contextType = ThemeContext;
-    render() {
-        const {isDarkMode, toggleTheme} = this.context;
-        const {classes} = this.props;
+function Navbar(props) {
+    // static contextType = ThemeContext;
+    // render() {
+        const {isDarkMode, toggleTheme} = useContext(ThemeContext);
+        const {language} = useContext(LanguageContext);
+        const {classes} = props;
         const {language} = this.props.languageContext;
         const {search, flag} = content[language];
         return(
@@ -63,6 +64,6 @@ class Navbar extends Component {
                 // )}
             // {/* </LanguageContext.Consumer> */}
         )
-    }
+    // }
 }; 
-export default withLanguageContext(withStyles(styles)(Navbar));
+export default withStyles(styles)(Navbar);
